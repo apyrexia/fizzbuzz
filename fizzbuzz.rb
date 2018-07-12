b0e0cad46f9fba0788bb7600ae681d289cbec398
Ethan Waterson <elwaterson@gmail.com>
2018-07-12 00:07:04 -0400

    Round 2, basic syntax fixes and reworked the iterator

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,18 +9,24 @@ https://blog.codinghorror.com/why-cant-programmers-program/
 
 
 #figuring it out on my own as I learn without looking up an answer
-#will need enumerator to work properly
+#as in not a direct answer, still using rubydoc etc
+#enjoying having a small problem to think through
+#that's in my league at this early stage
+
+#will need enumerator (iterator!) to work properly
 #learned a lot about conditionals from this already
 
-[1].map do { |n| n + 1 }
+sequence = (1..100).to_a
+
+sequence.each do |n|
 end
 
-if n % 3 = 0 && n % 5 = 0
-  puts fizzbuzz
-elsif n % 3 = 0 && n % 5 =/= 0
-  puts fizz
-elsif n % 3 =/= 0 && n % 5 = 0
-  puts buzz
+if (n % 3 = 0 && n % 5 = 0)
+  puts "fizzbuzz"
+elsif (n % 3 = 0 && n % 5 != 0)
+  puts "fizz"
+elsif (n % 3 != 0 && n % 5 = 0)
+  puts "buzz"
 else
   puts n
 end
